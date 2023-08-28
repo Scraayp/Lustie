33e5397f494beb8b08e1e81068666ca49e97d384
Scraayp <hello@michalk.nl>
2023-08-28 16:17:17 +0200

    feat: get, create tasks to internal storage

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.model';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
   @Get()
   getAllTasks() {
-    return 'Get all tasks';
+    this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    console.log(title);
+    return this.tasksService.createTask(title, description);
   }
 }
