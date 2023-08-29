a13dde44cc1e4fc3ca08509024ad97df58bfce16
Scraayp <hello@michalk.nl>
2023-08-29 12:40:31 +0200

    fix: Meta tags and features, include new logo

--- web/components/navbar.jsx
+++ web/components/navbar.jsx
@@ -28,7 +28,7 @@ const Nav = () => {
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src="/assets/images/logo.svg"
+          src="/assets/icons/icon.png"
           alt="logo"
           width={30}
           height={30}
